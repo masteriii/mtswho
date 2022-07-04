53e291ee1284d2cddf73d51ebad7ae703d84d0e1
Masteriii <masteriii_2@hotmail.com>
2022-07-04 13:00:31 +0700

    Add files via upload

--- image.php
+++ image.php
@@ -1,7 +1,6 @@
 <?php
 $fh = fopen('ip_list.txt', 'a');
-fwrite($fh, $_SERVER['REMOTE_ADDR']."
-");
+fwrite($fh, $_SERVER['REMOTE_ADDR']."");
 fclose($fh);
 $im = imagecreatefromjpeg("moo2.jpg");
 header('Content-Type: image/jpeg');
